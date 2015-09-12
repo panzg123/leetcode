268650935156111a863d6d8068167fe77bbb01ca
panzhengguang <zhengguangpan@hotmail.com>
2015-09-12 11:01:26 +0800

    update-09-12

---
+++ header.h
@@ -0,0 +1,15 @@
+#include<vector>
+#include<algorithm>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <numeric>
+#include <map>
+
+typedef struct ListNode
+{
+	int val;
+	ListNode* next;
+	ListNode(int x) :val(x), next(nullptr){}
+}ListNode;
+
