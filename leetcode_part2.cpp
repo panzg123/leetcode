52c4c10ce437d84f0ca5b51dc952271d1746e532
panzhengguang <zhengguangpan@hotmail.com>
2015-10-09 21:02:02 +0800

    update

--- leetcode_part2.cpp
+++ leetcode_part2.cpp
@@ -223,13 +223,209 @@ public:
 			return first;
 		}
 	};
+	/*
+	Search Insert Position
+	使用lower_bound，beat 0.01% cpp....
+	*/
+	int searchInsert(vector<int>& nums, int target)
+	{
+		return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+	}
+	/*
+	12ms, binary search, beats 0.47% of cpp submissions
+	*/
+	int searchInsert_v2(vector<int>& nums, int target)
+	{
+		auto first = nums.begin();
+		auto last = nums.end();
+		while (first != last)
+		{
+			auto mid = next(first, distance(first, last) / 2);
+			if (target > *mid) first = ++mid;
+			else last = mid;
+		}
+		return first-nums.begin();
+	}
+	/*
+	Search a 2D Matrix
+	二分查找,12ms
+	*/
+	bool searchMatrix(vector<vector<int>>& matrix, int target)
+	{
+		int rows = matrix.size();
+		int colums = matrix[0].size();
+		int length = rows*colums;
+		int first = 0, last = length - 1;
+		while (first <= last)
+		{
+			int mid = (first + last) / 2;
+			if (matrix[mid / colums][mid%colums] == target)
+				return true;
+			else if (matrix[mid / colums][mid%colums] < target)
+				first = mid + 1;
+			else
+				last = mid - 1;
+		}
+		return false;
+	}
+	/*
+	Search a 2D Matrix 2
+	900ms,beats 10% of cpp submissions
+	*/
+	bool searchMatrix_matrix2(vector<vector<int>>& matrix, int target)
+	{
+		int n = matrix[0].size();
+		for (size_t i = 0; i < matrix.size(); i++)
+		{
+			//剪枝
+			if (matrix[i][0] > target || matrix[i][n - 1] < target)
+				continue;
+			//二分查找
+			if (std::binary_search(matrix[i].begin(), matrix[i].end(), target))
+				return true;
+		}
+		return false;
+	}
+	/*
+	Search a 2D Matrix 2，复杂度为O(M+N)
+	272ms,beats 95%
+	*/
+	bool searchMatrix__matrix2_v2(vector<vector<int>>& matrix, int target) {
 
+		int row = 0;
+		int col = matrix[0].size() - 1;
+
+		while (row < matrix.size() && col >= 0) {
+			if (matrix[row][col] == target) return true;
+			if (matrix[row][col] < target) { row++; continue; }
+			if (matrix[row][col] > target) { col--; continue; }
+		}
+		return false;
+		      
+	}
+	/*
+	增量构造法，深搜，时间复杂度 O(2^n)，空间复杂度 O(n)
+	每个元素都有两种选择：加入或者不加入
+	*/
+	vector<vector<int>> subsets(vector<int> &nums)
+	{
+		sort(nums.begin(), nums.end());
+		vector<int> path;
+		vector<vector<int>> result;
+		subsets_help(result, path, 0, nums);
+		return result;
+
+	}
+	void subsets_help(vector<vector<int>> &result, vector<int> &path, int step, vector<int> nums)
+	{
+		if (step == nums.size())
+		{
+			result.push_back(path);
+			return;
+		}
+		//不选择
+		subsets_help(result, path, step + 1, nums);
+		path.push_back(nums[step]);
+		//选择
+		subsets_help(result, path, step + 1, nums);
+		path.pop_back();
+	}
+	/*Subsets II 用subsets的代码。
+	加入result之前进行判断是否存在*/
+	vector<vector<int>> subsetsWithDup(vector<int> &nums)
+	{
+		sort(nums.begin(), nums.end());
+		vector<int> path;
+		vector<vector<int>> result;
+		subsetsWithDup_help(result, path, 0, nums);
+		return result;
+
+	}
+	void subsetsWithDup_help(vector<vector<int>> &result, vector<int> &path, int step, vector<int> nums)
+	{
+		if (step == nums.size())
+		{
+			auto it = find(result.begin(), result.end(), path);
+			if (it == result.end())
+				result.push_back(path);
+			return;
+		}
+		//不选择
+		subsetsWithDup_help(result, path, step + 1, nums);
+		path.push_back(nums[step]);
+		//选择
+		subsetsWithDup_help(result, path, step + 1, nums);
+		path.pop_back();
+	}
+	/*Permutations
+	用next_permutation , 时间复杂度 O(n!)，空间复杂度 O(1)
+	*/
+	vector<vector<int>> permutation(vector<int>& nums)
+	{
+		vector<vector<int>> result;
+		sort(nums.begin(), nums.end());
+		do
+		{
+			result.push_back(nums);
+		} while (std::next_permutation(nums.begin(), nums.end()));
+		return result;
+	}
+	/*假如不能使用stl，可以自己实现next_permutation*/
+	template<typename BidiIt>
+	bool my_next_permutation(BidiIt first, BidiIt last)
+	{
+		// Get a reversed range to simplify reversed traversal.
+		const auto rfirst = reverse_iterator<BidiIt>(last);
+		const auto rlast = reverse_iterator<BidiIt>(first);
+		// Begin from the second last element to the first element.
+		auto pivot = next(rfirst);
+		// Find `pivot`, which is the first element that is no less than its
+		// successor. `Prev` is used since `pivort` is a `reversed_iterator`.
+		while (pivot != rlast && *pivot >= *prev(pivot))
+			++pivot;
+		// No such elemenet found, current sequence is already the largest
+		// permutation, then rearrange to the first permutation and return false.
+		if (pivot == rlast)
+		{
+			reverse(rfirst, rlast);
+			return false;
+		}
+		// Scan from right to left, find the first element that is greater than
+		// `pivot`.
+		auto change = find_if(rfirst, pivot, bind1st(less<int>(), *pivot));
+		swap(*change, *pivot);
+		reverse(rfirst, pivot);
+		return true;
+	}
+	/*Permutations
+	用next_permutation , 时间复杂度 O(n!)，空间复杂度 O(1)
+	next_permutation对于nums有重复数据也有效，不会生成重复排序
+	*/
+	vector<vector<int>> permutation_2(vector<int>& nums)
+	{
+		vector<vector<int>> result;
+		sort(nums.begin(), nums.end());
+		do
+		{
+			result.push_back(nums);
+		} while (std::next_permutation(nums.begin(), nums.end()));
+		return result;
+	}
 };
 int main()
 {
 
-	vector<int> vec = { 10, 20, 30, 30, 20, 10, 10, 20 };
-	std::sort(vec.begin(), vec.end());
+	vector<int> vec = { 1,2,2 };
 	Solution sol;
+	auto res = sol.permutation(vec);
+	for each (auto var in res)
+	{
+		for each (auto var1 in var)
+		{
+			cout << " " << var1;
+		}
+		cout << "\n";
+	}
+	system("pause");
 	return 0;
 }
