0686cda7721f1bb02c41d838d7fea051cfa5e0b4
panzhengguang <zhengguangpan@hotmail.com>
2015-10-09 09:32:18 +0800

    合并两个有序链表

--- leetcode_part2.cpp
+++ leetcode_part2.cpp
@@ -15,6 +15,77 @@ public:
 		while (ib >= 0)
 			nums1[icur--] = nums2[ib--];
 	}
+	/*Merge Two Sorted Lists 时间复杂度 O(min(m,n))，空间复杂度 O(1)*/
+	ListNode* mergeTwoLists(ListNode* l1, ListNode *l2)
+	{
+		if (l1 == nullptr) return l2;
+		if (l2 == nullptr) return l1;
+		ListNode head(-1);
+		ListNode *p = &head;
+		while (l1 != nullptr && l2 != nullptr)
+		{
+			if (l1->val < l2->val)
+			{
+				p->next = l1;
+				l1 = l1->next;
+			}
+			else
+			{
+				p->next = l2;
+				l2 = l2->next;
+			}
+			p = p->next;
+		}
+		p->next = l1 == nullptr ? l2 : l1;
+		return head.next;
+	}
+	/*Merge k Sorted Lists 两两合并超时,用优先队列*/
+	struct k_sort_compare {
+		bool operator()(const ListNode* l, const ListNode* r) {
+			return l->val > r->val;
+		}
+	};
+	ListNode* mergeKLists(vector<ListNode*>& lists)
+	{
+		if (lists.size() == 0) return NULL;
+
+		ListNode * head = NULL;
+		ListNode * ptr = NULL;
+		vector<int> null_index;
+		for (int i = 0; i<lists.size(); i++)
+		{
+			if (lists[i] == NULL) null_index.push_back(i);
+		}
+		cout << null_index.size() << endl;
+		for (int i = null_index.size() - 1; i<null_index.size(); i--)
+		{
+			lists.erase(lists.begin() + null_index[i]);
+		}
+		priority_queue<ListNode*, vector<ListNode*>, k_sort_compare> pq;
+		for (int i = 0; i<lists.size(); i++)
+		{
+			pq.push(lists[i]);
+		}
+		while (pq.size() > 0)
+		{
+			ListNode * m = pq.top();
+			pq.pop();
+			if (m->next != NULL) pq.push(m->next);
+			m->next = NULL;
+			if (head == NULL)
+			{
+				head = m;
+				ptr = head;
+			}
+			else
+			{
+				ptr->next = m;
+				ptr = ptr->next;
+			}
+
+		}
+		return head;
+	}
 };
 int main()
 {
