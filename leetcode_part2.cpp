8b484e9f272719af0b0f8abb3ce988964f63ab0e
panzhengguang <zhengguangpan@hotmail.com>
2015-10-12 21:19:28 +0800

    update  to best time to buy and sell

--- leetcode_part2.cpp
+++ leetcode_part2.cpp
@@ -1302,9 +1302,53 @@ public:
 	}
 
 	/*Best Time to Buy and Sell Stock IV*/
-	int maxProfit(int k, vector<int>& prices)
+	int maxProfit4(int k, vector<int>& prices)
 	{
 
+	}
+	/*Longest Substring Without Repeating Characters*/
+	int lengthOfLongestSubstring(string s)
+	{
+		int length = s.size();
+		if (length == 0) return 0;
+		string temp;  //保存路径
+		int max_len = 0;
+		for (size_t i = 0; i < length; i++)
+		{
+			//查找位置
+			auto find_res = find(temp.begin(), temp.end(), s[i]);
+			//不重复
+			if (find_res == temp.end())
+			{
+				temp.push_back(s[i]);
+			}
+			//重复，去除该重复字母之前的子串，并添加该字母追加到末尾
+			else
+			{
+				max_len = max(max_len,(int)temp.size());
+				temp.erase(temp.begin(), find_res+1);
+				temp.push_back(s[i]);
+			}
+		}
+		//字符串s本身的长度
+		return max_len = max(max_len, (int)temp.size());
+	}
+	/*Container With Most Water 贪心 双指针*/
+	int maxArea(vector<int> &height)
+	{
+		int start = 0;
+		int end = height.size() - 1;
+		int result = INT_MIN;
+		while (start < end) 
+		{
+			int area = min(height[end], height[start]) * (end - start);
+			result = max(result, area);
+			if (height[start] <= height[end]) 
+				start++;
+			else 
+				end--;
+		}
+		return result;
 	}
 private:
 	/*Surrounded Regions BFS*/
@@ -1376,7 +1420,7 @@ int main()
 
 	Solution sol;
 	vector<int> vec = { 3, 2, 1, 0, 4 };
-	auto res = sol.maxProfit(vec);
+	auto res = sol.lengthOfLongestSubstring("abcabcbb");
 	cout << res;
 	/*for each (auto var in res)
 	{
