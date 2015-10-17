db76572bc0ef004b9587eaf2f3dbedef4f572eb3
panzhengguang <zhengguangpan@hotmail.com>
2015-10-17 16:39:28 +0800

    求子串个数，DP

--- Distinct_Subsequences.cpp
+++ Distinct_Subsequences.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
 	/*Distinct Subsequences
-	 二维动规 + 滚动数组
-	 时间复杂度 O(m*n)，空间复杂度 O(n)
+	 二维动规 + 滚动数组,时间复杂度 O(m*n)，空间复杂度 O(n)
+	 思路：设状态为 f(i, j)，表示 T[0,j] 在 S[0,i] 里出现的次数。
+	 首先，无论 S[i] 和 T[j] 是否相等，若不使用 S[i]，则 f(i, j) = f(i − 1, j)；
+	 若 S[i]==T[j]，则可以使用 S[i]，此时f(i, j) = f(i − 1, j) + f(i − 1, j − 1)。
 	*/
     int numDistinct(string s, string t) {
         vector<int> f(t.size() + 1);
