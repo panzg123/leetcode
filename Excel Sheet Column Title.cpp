aa49cdf275236c6363ead011ab36b630313a7b17
panzhengguang <zhengguangpan@hotmail.com>
2015-11-19 20:28:17 +0800

    26进制

--- Excel Sheet Column Title.cpp
+++ Excel Sheet Column Title.cpp
@@ -1,13 +1,15 @@
-class Solution {
+class Solution
+{
 public:
-    string convertToTitle(int n) {
-            string s;
-			while (n)
-			{
-				int a = (n-1)% 26;
-				s.insert(s.begin(), 'A'+a);
-				n=(n-1)/26;
-			}
-			return s;
+    string convertToTitle(int n)
+    {
+        string s;
+        while (n)
+        {
+            int a = (n-1)% 26;
+            s.insert(s.begin(), 'A'+a);
+            n=(n-1)/26;
+        }
+        return s;
     }
 };
