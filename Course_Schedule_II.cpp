c67c048b4ffa4f6c925c262196bd9a29ef5f9142
panzhengguang <panzg123@qq.com>
2016-01-02 15:50:04 +0800

    拓扑排序

---
+++ Course_Schedule_II.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<pair<int, int>>& prerequisites) {
+            vector<int> in_degree(numCourses, 0); //存储入度
+			vector<unordered_set<int>> matrix(numCourses);//存储图
+			vector<int> res;
+
+			for (int i = 0; i < prerequisites.size(); i++)
+			{
+				matrix[prerequisites[i].second].insert(prerequisites[i].first);
+			}
+			//计算入度
+			for (int i = 0; i < numCourses; i++)
+			{
+				for (auto it = matrix[i].begin(); it != matrix[i].end(); it++)
+				{
+					in_degree[*it]++;
+				}
+			}
+			stack<int> zeor_degree_stack;//存储入度为0的节点
+			int count = 0;//入度0的节点计数器
+			for (int i = 0; i < numCourses; i++)
+			{
+				if (in_degree[i] == 0)
+				{
+					zeor_degree_stack.push(i);
+					count++; //入度为0，计数加1
+					res.push_back(i);
+				}
+			}
+			//循环抽取入度为0的节点
+			while (!zeor_degree_stack.empty())
+			{
+				int top = zeor_degree_stack.top();
+				zeor_degree_stack.pop();
+				for (auto it = matrix[top].begin(); it != matrix[top].end(); it++)
+				{
+					in_degree[*it]--;
+					if (in_degree[*it] == 0)
+					{
+						zeor_degree_stack.push(*it);
+						count++;
+						res.push_back(*it);
+					}
+				}
+			}
+			if (count == numCourses)
+				return res;
+			else
+			{
+				vector<int> tmp; return tmp;
+			}
+    }
+};
