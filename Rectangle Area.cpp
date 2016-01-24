2cc2cdfc6a224a202f5315e6a187d52108786758
panzhengguang <panzg123@qq.com>
2016-01-24 14:58:45 +0800

    math

---
+++ Rectangle Area.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+		//Rectangle Area,求两个矩形的全部覆盖面积
+        int computeArea(int A, int B, int C, int D, int E, int F, int G, int H)
+		{
+			int x = get_union(A, C, E, G);
+			int y = get_union(B, D, F, H);
+
+			int area1 = abs((A - C)*(B - D));
+			int area2 = abs((E - G)*(F - H));
+			return area1 + area2 - x*y;
+		}
+		//求区间[x1,x2]与区间[x3,x4]之间的交集距离
+		int get_union(int x1, int x2, int x3, int x4)
+		{
+			if (x1 <= x3 && x2 >= x4)
+				return x4 - x3;
+			else if (x1 <= x3 && x2 <= x4 && x2 <= x3)
+				return 0;
+			else if (x1 <= x3 && x2 <= x4 && x2 >= x3)
+				return x2 - x3;
+			else if (x1 >= x3 && x2 <= x4)
+				return x2 - x1;
+			else if (x1 >= x3 && x2 >= x4 && x1 >= x4)
+				return 0;
+			else
+				return x4 - x1;
+		}
+};
