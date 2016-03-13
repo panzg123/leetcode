78a4250b9301854367ee43a81ebb7d9dfd8dc956
panzhengguang <panzg123@qq.com>
2016-03-13 09:51:43 +0800

    add comment

--- leetcode_part4.cpp
+++ leetcode_part4.cpp
@@ -1353,17 +1353,20 @@ namespace panzg_leetcode
 			}
 			return start;
 		}
-		//时间复杂度为O(N)，具体参考：《面试指南》P386
+		//时间复杂度为O(N)，具体参考：《面试指南》P386，l表示目前出现的数的范围[0,l],r表示可能出现的最大数值
 		int missingNumber_v2(vector<int>& nums)
 		{
 			int l = 0;
 			int r = nums.size();
 			while (l<r)
 			{
+				//正常出现
 				if (nums[l] == l)
 					l++;
+				//重复出现[0,l]的某数;或直接超出最大值；或者重复出现[l,r]的某数,此时都应该缩小r
 				else if (nums[l] <= l-1 || nums[l]>r-1 || nums[nums[l]] == nums[l])
 					nums[l] = nums[--r];
+				//出现[l+1,r]之间的数，只是顺序不正确，此时需要交换位置
 				else
 					swap(nums[l], nums[nums[l]]);
 			}
